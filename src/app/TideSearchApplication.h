4c579309cb84cbb34288957aaafc0ec809ecc1eb
Azamature1719 <ismagulovazamat.g@gmail.com>
2023-04-13 15:15:30 +0300

    GPU Macros added

--- src/app/TideSearchApplication.h
+++ src/app/TideSearchApplication.h
@@ -14,11 +14,21 @@
 #include "tide/max_mz.h"
 #include "util/MathUtil.h"
 
+#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
+
+#define GPUscoring() __global__ void gpuscoring(int a, int b, int *c){ \
+   int id = threadIDx.x; \
+   c[id] = a + b; \
+}
+
+
 using namespace std;
 
 /**
  * Locks for multi-threading in Tide.
  */
+
 enum _tide_search_lock {
   LOCK_RESULTS,       // Results file output
   LOCK_CASCADE,       // Only used by cascade-search on spectrum_flag (map)
@@ -42,16 +52,16 @@ struct InputFile {
 };
 
 struct ScSortByMz {
-  explicit ScSortByMz(double precursor_window) { precursor_window_ = precursor_window; }
-  bool operator() (const SpectrumCollection::SpecCharge x, const SpectrumCollection::SpecCharge y) {
-      return (x.spectrum->PrecursorMZ() - MASS_PROTON - precursor_window_) * x.charge <
-             (y.spectrum->PrecursorMZ() - MASS_PROTON - precursor_window_) * y.charge;
-  }
-  double precursor_window_;
+    explicit ScSortByMz(double precursor_window) { precursor_window_ = precursor_window; }
+    bool operator() (const SpectrumCollection::SpecCharge x, const SpectrumCollection::SpecCharge y) {
+        return (x.spectrum->PrecursorMZ() - MASS_PROTON - precursor_window_) * x.charge <
+				(y.spectrum->PrecursorMZ() - MASS_PROTON - precursor_window_) * y.charge;
+    }
+    double precursor_window_;
 };
 
 class TideSearchApplication : public CruxApplication {
-  private:
+private:
   //Added by Andy Lin in Feb 2016
   //function determines which mass bin a precusor mass is in
   void getMassBin (
@@ -125,6 +135,7 @@ class TideSearchApplication : public CruxApplication {
     const vector<SpectrumCollection::SpecCharge>* spec_charges,
     vector<ActivePeptideQueue*> active_peptide_queue,
     ProteinVec& proteins,
+    vector<const pb::AuxLocation*>& locations,
     double precursor_window,
     WINDOW_TYPE_T window_type,
     double spectrum_min_mz,
@@ -132,6 +143,7 @@ class TideSearchApplication : public CruxApplication {
     int min_scan,
     int max_scan,
     int min_peaks,
+    int search_charge,
     int top_matches,
     double highest_mz,
     ofstream* target_file,
@@ -262,6 +274,7 @@ class TideSearchApplication : public CruxApplication {
     const vector<SpectrumCollection::SpecCharge>* spec_charges;
     ActivePeptideQueue* active_peptide_queue;
     ProteinVec proteins;
+    vector<const pb::AuxLocation*> locations;
     double precursor_window;
     WINDOW_TYPE_T window_type;
     double spectrum_min_mz;
@@ -269,6 +282,7 @@ class TideSearchApplication : public CruxApplication {
     int min_scan;
     int max_scan;
     int min_peaks;
+    int search_charge;
     int top_matches;
     double highest_mz;
     ofstream* target_file;
@@ -296,13 +310,14 @@ class TideSearchApplication : public CruxApplication {
     bool exact_pval_search;
     map<pair<string, unsigned int>, bool>* spectrum_flag;
     int* sc_index;
-    long* total_candidate_peptides;
+    int* total_candidate_peptides;
     vector<int>* negative_isotope_errors;
 
     thread_data (const string& spectrum_filename_, const vector<SpectrumCollection::SpecCharge>* spec_charges_,
-            ActivePeptideQueue* active_peptide_queue_, ProteinVec proteins_, double precursor_window_,
+            ActivePeptideQueue* active_peptide_queue_, ProteinVec proteins_,
+            vector<const pb::AuxLocation*> locations_, double precursor_window_,
             WINDOW_TYPE_T window_type_, double spectrum_min_mz_, double spectrum_max_mz_,
-            int min_scan_, int max_scan_, int min_peaks_, int top_matches_,
+            int min_scan_, int max_scan_, int min_peaks_, int search_charge_, int top_matches_,
             double highest_mz_, ofstream* target_file_,
             ofstream* decoy_file_, bool compute_sp_, int64_t thread_num_, int64_t num_threads_, int nAA_,
             double* aaFreqN_, double* aaFreqI_, double* aaFreqC_, int* aaMass_, int nAARes_,
@@ -310,12 +325,12 @@ class TideSearchApplication : public CruxApplication {
             const vector<double>* dAAFreqC_, const vector<double>* dAAMass_,
             const pb::ModTable* mod_table_, const pb::ModTable* nterm_mod_table_, const pb::ModTable* cterm_mod_table_, const int decoysPerTarget_,
             vector<boost::mutex*> locks_array_, double bin_width_, double bin_offset_, bool exact_pval_search_,
-            map<pair<string, unsigned int>, bool>* spectrum_flag_, int* sc_index_, long* total_candidate_peptides_,
+            map<pair<string, unsigned int>, bool>* spectrum_flag_, int* sc_index_, int* total_candidate_peptides_,
             vector<int>* negative_isotope_errors_) :
             spectrum_filename(spectrum_filename_), spec_charges(spec_charges_), active_peptide_queue(active_peptide_queue_),
-            proteins(proteins_), precursor_window(precursor_window_), window_type(window_type_),
+            proteins(proteins_), locations(locations_), precursor_window(precursor_window_), window_type(window_type_),
             spectrum_min_mz(spectrum_min_mz_), spectrum_max_mz(spectrum_max_mz_), min_scan(min_scan_), max_scan(max_scan_),
-            min_peaks(min_peaks_), top_matches(top_matches_), highest_mz(highest_mz_),
+            min_peaks(min_peaks_), search_charge(search_charge_), top_matches(top_matches_), highest_mz(highest_mz_),
             target_file(target_file_), decoy_file(decoy_file_), compute_sp(compute_sp_),
             thread_num(thread_num_), num_threads(num_threads_), nAA(nAA_), aaFreqN(aaFreqN_), aaFreqI(aaFreqI_), aaFreqC(aaFreqC_),
             aaMass(aaMass_), nAARes(nAARes_), dAAFreqN(dAAFreqN_), dAAFreqI(dAAFreqI_), dAAFreqC(dAAFreqC_), dAAMass(dAAMass_),
